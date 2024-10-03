c3ad055c8f1ff4921b58531c16947e84451ae405
Pascal Kraupner <pascal.kraupner@gmail.com>
2024-10-03 18:01:03 +0200

    working on version 2.0, adding meta information

--- src/components/Projects/Projects.vue
+++ src/components/Projects/Projects.vue
@@ -3,38 +3,38 @@ import Heading from "../../components/Headings/Heading.vue";
 import Section from "../../components/Section.vue";
 import ProjectCard from "./ProjectCard/ProjectCard.vue";
 const projects = [
-    {
-        id: 1,
-        name: "Enchantertain",
-        livePreviewLink: "https://www.enchantertain.com/",
-        githubRepositoryLink:
-            "https://github.com/PascalKraupner/enchantertain",
-        cardImage: {
-            src: "/img/projects/enchantertain_preview.jpg",
-            alt: "enchantertain preview",
-        },
-        shortDescription: `In diesem Projekt habe ich eine kleine Laravel-Anwendung entwickelt, mit der nach Filmen, Serien oder Spielen gesucht werden kann. Dank künstlicher Intelligenz werden passende Vorschläge für ähnliche Inhalte generiert. Die App ermöglicht es, Inhalte zu entdecken, die genau den Vorlieben und Interessen des Nutzers entsprechen. Mein Ziel ist es, das Projekt kontinuierlich zu verbessern und um neue Features zu erweitern.`,
-        technologyStack: ["laravel", "tailwind", "livewire", "alpine"],
+  {
+    id: 1,
+    name: "Enchantertain",
+    livePreviewLink: "https://www.enchantertain.com/",
+    githubRepositoryLink:
+      "https://github.com/PascalKraupner/enchantertain",
+    cardImage: {
+      src: "/img/projects/enchantertain_preview.jpg",
+      alt: "enchantertain preview",
     },
+    shortDescription: `In diesem Projekt habe ich eine kleine Laravel-Anwendung entwickelt, mit der nach Filmen, Serien oder Spielen gesucht werden kann. Dank künstlicher Intelligenz werden passende Vorschläge für ähnliche Inhalte generiert. Die App ermöglicht es, Inhalte zu entdecken, die genau den Vorlieben und Interessen des Nutzers entsprechen. Mein Ziel ist es, das Projekt kontinuierlich zu verbessern und um neue Features zu erweitern.`,
+    technologyStack: ["laravel", "tailwind", "livewire", "alpine"],
+  },
 ];
 </script>
 
 <template>
-    <Section id="projects">
-        <Heading :level="2" :size="1" text="Projekte" :cursor="true" />
+  <Section id="projects">
+    <Heading :level="2" :size="1" text="Projekte" :cursor="true" />
 
-        <!-- set count var and try different values for number of cards and try mobile view, cards should
+    <!-- set count var and try different values for number of cards and try mobile view, cards should
         looks always good -->
 
-        <div class="grid grid-auto-fit-xl gap-8 my-16 place-items-center">
-            <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
-        </div>
+    <div class="grid grid-auto-fit-xl gap-8 my-16 place-items-center">
+      <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+    </div>
 
-        <!--
+    <!--
     TODO: Implement dedicated projects detail page
             <div class="flex justify-center mb-20">
                 <PrimaryButton text="Alle Projekte" href="/projects" icon="diagram-project" />
             </div>
     -->
-    </Section>
+  </Section>
 </template>
